4924452a64de40ee48cb8c4b2845200fb02c2f50
Tatiana <ganzhinovat@yandex.ru>
2020-08-07 14:32:01 +0300

    Add animation and modal window

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* VIDEO EARTH */
+
 var instance = new vidbg('.video', {
     mp4: 'video/world.mp4', // URL or relative path to MP4 video
     webm: 'video/world.webm', // URL or relative path to webm video
@@ -6,3 +8,23 @@ var instance = new vidbg('.video', {
   });
 
   var rellax = new Rellax('.rocket');
+
+/* FORM REGISTRATION OPEN/CLOSE */
+
+var footerBtn = document.querySelector('.footer-btn');
+var modal = document.querySelector('.modal');
+var close = document.querySelector('.close-icon');
+
+
+footerBtn.addEventListener('click', () => {
+        modal.classList.add('active');
+    });
+    
+    close.addEventListener('click', () => {
+        modal.classList.remove('active');
+});
+
+
+/* AOS ANIMATION */
+
+AOS.init();
